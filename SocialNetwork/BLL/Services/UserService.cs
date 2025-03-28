940ebdc3cda548a53ba42532607de418e6a4d367
1speqller <hoshigake2000@gmail.com>
2025-03-28 12:37:54 +0300

    Добавьте файлы проекта.

---
+++ SocialNetwork/BLL/Services/UserService.cs
@@ -0,0 +1,103 @@
+﻿using SocialNetwork.BLL.Exceptions;
+using SocialNetwork.BLL.Models;
+using SocialNetwork.DAL.Entities;
+using SocialNetwork.DAL.Repositories;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SocialNetwork.BLL.Services
+{
+    public class UserService
+    {
+        IUserRepository userRepository;
+        public UserService()
+        {
+            userRepository = new UserRepository();
+        }
+
+        public void Register(UserRegistrationData userRegistrationData)
+        {
+            if (String.IsNullOrEmpty(userRegistrationData.FirstName))
+                throw new ArgumentNullException();
+
+            if (String.IsNullOrEmpty(userRegistrationData.LastName))
+                throw new ArgumentNullException();
+
+            if (String.IsNullOrEmpty(userRegistrationData.Password))
+                throw new ArgumentNullException();
+
+            if (String.IsNullOrEmpty(userRegistrationData.Email))
+                throw new ArgumentNullException();
+
+            if (userRegistrationData.Password.Length < 8)
+                throw new ArgumentNullException();
+
+            if (!new EmailAddressAttribute().IsValid(userRegistrationData.Email))
+                throw new ArgumentNullException();
+
+            if (userRepository.FindByEmail(userRegistrationData.Email) != null)
+                throw new ArgumentNullException();
+
+            var userEntity = new UserEntity()
+            {
+                FirstName = userRegistrationData.FirstName,
+                LastName = userRegistrationData.LastName,
+                Password = userRegistrationData.Password,
+                Email = userRegistrationData.Email
+            };
+
+            if (this.userRepository.Create(userEntity) == 0)
+                throw new Exception();
+        }
+
+        public User Authenticate(UserAuthenticationData userAuthenticationData)
+        {
+            var findUserEntity = userRepository.FindByEmail(userAuthenticationData.Email);
+            if (findUserEntity is null) throw new UserNotFoundException();
+
+            if (findUserEntity.Password != userAuthenticationData.Password)
+                throw new WrongPasswordException();
+
+            return ConstructUserModel(findUserEntity);
+        }
+
+        public User FindByEmail(string email)
+        {
+            var findUserEntity = userRepository.FindByEmail(email);
+            if (findUserEntity is null) throw new UserNotFoundException();
+
+            return ConstructUserModel(findUserEntity);
+        }
+
+        public void Update(User user)
+        {
+            var updatableUserEntity = new UserEntity()
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Password = user.Password,
+                Email = user.Email,
+                Photo = user.Photo,
+                FavoriteMovie = user.FavoriteMovie,
+                FavoriteBook = user.FavoriteBook
+            };
+
+            if (this.userRepository.Update(updatableUserEntity) == 0)
+                throw new Exception();
+        }
+
+        private User ConstructUserModel(UserEntity userEntity)
+        {
+            return new User(userEntity.Id,
+                          userEntity.FirstName,
+                          userEntity.LastName,
+                          userEntity.Password,
+                          userEntity.Email,
+                          userEntity.Photo,
+                          userEntity.FavoriteMovie,
+                          userEntity.FavoriteBook);
+        }
+    }
+
+}
